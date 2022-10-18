845fcd66190856802e3857e0542561db5cb70721
Gabriel Fernandes <fernandes.gh@outlook.com>
2022-10-18 16:25:57 -0300

    feat: get restaurantes & get pratos da API

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import IPrato from '../../../interfaces/IPrato';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
@@ -8,12 +11,26 @@ interface RestauranteProps {
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
 
+  const [ pratos, setPratos ] = useState<IPrato[]>();
+  const [ proximaPagina, setProximaPagina] = useState('');
+
+  useEffect(() => {
+    axios.get<IPrato[]>(`http://0.0.0.0:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then((resp) => {
+        console.log(resp.data);
+        setPratos(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [restaurante.id]);
+
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
   </section>)
 }
